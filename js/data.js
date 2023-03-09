01209d3a32077fcbac9909383dbebe5fdcf96bd0
Axel <adalvarez@scl.edu.gt>
2023-03-08 21:16:41 -0600

    Ya no puedo con mi vd

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let data_info = [
+
+    {
+        id : "1",
+        titulo : "git add .",
+        mensaje : "Organizar y gabeta",
+        estado: true
+    },
+    {
+        id : "2",
+        titulo : "git commit -m",
+        mensaje : "Preguntar por mis herramientas",
+        estado: true
+    },
+    {
+        id : "3",
+        titulo : "git pull",
+        mensaje : "Lavar el carro",
+        estado: true
+    },
+    {
+        id : "4",
+        titulo : "git push",
+        mensaje : "Comer 1 fruta",
+        estado: true
+    },
+
+];
+
+export {data_info};
